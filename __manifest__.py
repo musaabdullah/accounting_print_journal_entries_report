3218d4aeee74d8a8511a5a10f85adf5e9d716b65
Musa Abdullah <60151264+musaabdullah@users.noreply.github.com>
2023-11-11 18:05:00 +0300

    update accounting print journal entry

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Accounting Print Journal Entries Report in Odoo',
+    'version': '16.0.0.2',
+    'category': 'Account',
+    'license': 'OPL-1',
+    'summary': 'Allow to print pdf report of Journal Entries in Accounting.',
+    'description': """
+    Allow to print pdf report of Journal Entries.
+    print journal entry accounting
+    print journal entry reports
+    account journal entry reports
+    accounting entry reports
+""",
+    'author': 'Musa Abdullah',
+    'depends': ['base','account'],
+    'data': [
+            'report/accounting_print_report_journal_entries.xml',
+            'report/accounting_print_report_journal_entries_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    "images":["static/description/Banner.gif"],
+}
+
